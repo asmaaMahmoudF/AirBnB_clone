a11f9eaa1fd5874c2de7ab243f8df1927b459459
vulturepremier <vulturepremier@gmail.com>
2024-05-20 02:40:02 +0300

    Commit

--- models/base_model.py
+++ models/base_model.py
@@ -33,15 +33,3 @@ class BaseModel:
         """overriding the __str__ method to print specific format"""
         return "[{}] ({}) {}".format(type(self).__name__,self.id, self.__dict__)
 
-my_model = BaseModel()
-my_model.name = "My_First_Model"
-my_model.my_number = 89
-print(my_model.id)
-print(my_model)
-print(type(my_model.created_at))
-print("--")
-my_model_json = my_model.to_dict()
-print(my_model_json)
-print("JSON of my_model:")
-for key in my_model_json.keys():
-    print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
