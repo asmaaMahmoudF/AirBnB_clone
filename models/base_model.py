51311c4b2e5dfb21adbc4aea7029e47918da7de5
asmaaMahmoudF <asmaafahmy932@gmail.com>
2024-05-22 22:17:33 +0300

    update

--- models/base_model.py
+++ models/base_model.py
@@ -26,7 +26,6 @@ class BaseModel:
         """Update the time of instance creation"""
 
         self.updated_at = datetime.now()
-        models.storage.save()
     def to_dict(self):
         """A function to collect the needed data and put them into dic
         by making a copy of __dict__ using {**self}"""
