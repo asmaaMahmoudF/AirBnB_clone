ba699f7bb8a23240a5fcee1abd1993b9271e4c4b
asmaaMahmoudF <asmaafahmy932@gmail.com>
2024-05-23 05:18:19 +0300

    consol

--- console.py
+++ console.py
@@ -4,12 +4,11 @@
 '''
 
 import cmd
-from models import storage
+import models
 from models.base_model import BaseModel
 from models.state import State
 from models.review import Review
 from models.city import City
-from models.amenity import amenity
 from models.place import Place
 from models.user import User
 
@@ -20,74 +19,33 @@ class HBNBCommand(cmd.Cmd):
     prompt = '(hbnb)'
 
     classes = {
-        'BaseModel': BaseModel.BaseModel,
+        'BaseModel': BaseModel,
         "User": User,
         'Place': Place,
         'State': State,
         'City': City,
-        'Amenity':amenity.Amenity,
         'Review': Review,
         'State': State,
         'City': City,
     }
 
-    def do_update(self, arg):
-        return True
-
-    def do_quit(self, arg):
-        '''Quit command to exit the program'''
-        return True
-
     def do_EOF(self, arg):
         '''exit the cmd iteration'''
         return True
 
-    def emptyline(self):
-        pass
-
-    def do_create(self, arg):
-        '''Creates a new instance of BaseModel, saves it'''
-        args_list = arg.split()
-        classes = {
-        'BaseModel': BaseModel.BaseModel,
-        "User": User,
-        'Place': Place,
-        'State': State,
-        'City': City,
-        'Amenity':amenity.Amenity,
-        'Review': Review,
-        'State': State,
-        'City': City
-       }
-        if len(args_list) > 1:
-            print(f"unknown number of args {args}")
-            print(f"Usage: create <class-name>")
-        elif len(args_list) == 0:
-            print('** class name missing **')
-        elif args_list[0] not in classes.keys():
-            print('** class doesn\'t exist **')
-        else:
-            instance = classes[args_list[0]]()
-            print(instance.id)
-            instance.save()
-
-    def do_show(self, arg):
-        return True
-
-    def do_destroy(self, arg):
-        pass
-
-    def do_all(self, arg):
-        pass
-
     def do_help(self, arg):
         return super().do_help(arg)
 
-    def help_EOF(self):
-        print("EOF signal to exit the program.")
+    def do_quit(self, arg):
+        return True
 
     def help_quit(self):
-        print("Quit command to exit the program.")   
+        '''help the quit command'''
+        print("Quit command to exit the program.")
+
+    def emptyline(self):
+        '''do nothing when an empty line is entered'''
+        pass
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
